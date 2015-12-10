e2d6b48f705444d95b34c6b8b043134e60b72cd0
Christine Yu <internalfire5@live.com>
2015-12-10 13:19:46 -0800

    Submission for CodeFest

--- views/default/admin/administer_utilities/disk_space_usage.php
+++ views/default/admin/administer_utilities/disk_space_usage.php
@@ -4,9 +4,10 @@
 elgg_load_library('jgraph');
 elgg_load_library('jgraph_pie');
 elgg_load_library('jgraph_bar');
+elgg_load_css('collap_tree');
 
-
-
+// colours
+$pie_colours_var = array('#800000','#339933','#99ff66','#ff6699','#0099ff','#002e4d','#990099', '#003300');
 
 // get all site guid (since there are subsites)
 $query = 'SELECT DISTINCT site_guid FROM elgg_entities';
@@ -74,9 +75,9 @@ if ($num_of_file > 0) {
 	$p1->SetColor('black');
 	$p1->SetSliceColors(array('#800000','#339933'));
 
-	$fp = elgg_get_plugins_path()."c_dashboard_usage/img/pie_chart1.png";
+	$fp = elgg_get_plugins_path()."c_dashboard_usage/img/pie_chart.png";
 	$file = $graph->Stroke($fp);
-	echo '<img src="'.elgg_get_site_url().'mod/c_dashboard_usage/img/pie_chart1.png">';
+	echo '<img src="'.elgg_get_site_url().'mod/c_dashboard_usage/img/pie_chart.png">';
 } else {
 	echo 'This site has no files uploaded!<br/>';
 }
@@ -98,12 +99,20 @@ $site_entities = array("Sites","Groups","Users");
 echo "<hr>";
 
 
-// sites
+// SITES AND SUBSITES
 echo $site_entities[0];
 echo "<hr>";
-foreach ($elgg_sites as $elgg_site) {
-	echo "..<strong>".(int)$elgg_site->guid." - ".$elgg_site->name."</strong><br/>";
 
+
+
+
+echo '<ol class="tree">';
+
+foreach ($elgg_sites as $elgg_site) {
+	echo '<li>';
+	//echo "..<strong>".(int)$elgg_site->guid." - ".$elgg_site->name."</strong><br/>";
+	echo '<label>'."..<strong>".(int)$elgg_site->guid." - ".$elgg_site->name."</strong>".'</label><input type="checkbox" id="folder1" />';
+	$ext_count = array();
 	// get all files
 	$elgg_files = elgg_get_entities(array(
 		'subtype' => 'file',
@@ -111,21 +120,89 @@ foreach ($elgg_sites as $elgg_site) {
 		'site_guid' => $elgg_site->guid,
 	));
 
+	echo '<ol>';
+	
 	foreach ($elgg_files as $elgg_file) {
 		$url = elgg_add_action_tokens_to_url(elgg_get_site_url().'action/file/delete?guid='.$elgg_file->getGUID());
-		echo '......'.$elgg_file->getFilename()." | ".$elgg_file->size()." bytes"." <a href='".$url."'> X </a><br/>"; // make sure after deletion, forward url back to this admin page...
-		echo '..........'.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated()."<br/>"; // time is displayed in epoch time
+		$ext = pathinfo($elgg_file->getFilename(), PATHINFO_EXTENSION);
+		//echo '......'.$elgg_file->getFilename()." | ".$elgg_file->size()." bytes"." <a href='".$url."'> X </a><br/>"; // make sure after deletion, forward url back to this admin page...
+		//echo '..........'.$ext.' | '.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated()."<br/>"; // time is displayed in epoch time
+		echo '<li>';
+		echo '<label for="subsubfolder2">'.'......'.$elgg_file->getFilename()." | ".$elgg_file->size()." bytes"." <a href='".$url."'> X </a>".'</label> <input type="checkbox" id="subsubfolder2" />';
+			echo '<ol>';
+				echo '<li class="file">'.'..........'.$ext.' | '.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated().'</li>';
+			echo '</ol>';
+		echo '</li>';
+		
+		if (!$ext_count[$ext])
+			$ext_count[$ext] = 1;
+		else
+			$ext_count[$ext]++;
 	}
-}
+
+	
+	echo '</ol>';
+
+
+	// CREATE PIE CHART FOR VISUAL
+	//$data = array($pleio_main_size,$subsites_size,$groups_size,$users_size);
+	//$data = array($space_used, $total_space_avail);
+	$ext_name = array();
+	$data = array();
+	foreach ($ext_count as $extension => $total_ext) {
+		$ext_name[] = $extension."\n%.1f%%";
+		$data[] = $total_ext;
+	}
+
+
+	if ($data) {
+		// Create the Pie Graph. 
+		$graph = new PieGraph(350,250);
+
+		$theme_class="DefaultTheme";
+		//$graph->SetTheme(new $theme_class());
+
+		// Set A title for the plot
+		$graph->title->Set("");
+		$graph->SetBox(true);
+
+		// Create
+		$p1 = new PiePlot($data);
+		$p1->SetLabelType(PIE_VALUE_PER);
+		$p_lbl = $ext_name;
+		$p1->SetLabels($p_lbl);
+		$graph->Add($p1);
+
+		$p1->ShowBorder();
+		$p1->SetColor('black');
+		$p1->SetSliceColors(array('#800000','#339933','#99ff66','#ff6699','#0099ff'));
+
+		$fp = elgg_get_plugins_path()."c_dashboard_usage/img/pie_chart-".$num_pie.".png";
+		$file = $graph->Stroke($fp);
+		echo '<br/><img src="'.elgg_get_site_url().'mod/c_dashboard_usage/img/pie_chart-'.$num_pie.'.png"><br/>';
+		$num_pie++;
+	} else {
+		echo '<center><strong><p>No Data Available</p></strong></center>';
+	}
+
+	echo '</li>';
+
+} // end loop for each site
+
+echo '</ol>';
+
+
 echo "<hr>";
 
 
-// groups
+
+
+// GROUPS
 echo $site_entities[1];
 echo "<hr>";
 foreach ($elgg_groups as $elgg_group) {
 	echo "..<strong>".(int)$elgg_group->guid." - ".$elgg_group->name."</strong><br/>";
-
+	$ext_count = array();
 	// get all files
 	$elgg_files = elgg_get_entities(array(
 		'subtype' => 'file',
@@ -134,13 +211,62 @@ foreach ($elgg_groups as $elgg_group) {
 	));
 
 	foreach ($elgg_files as $elgg_file) {
+		$url = elgg_add_action_tokens_to_url(elgg_get_site_url().'action/file/delete?guid='.$elgg_file->getGUID());
+		$ext = pathinfo($elgg_file->getFilename(), PATHINFO_EXTENSION);
 		if (($elgg_file->getContainerEntity()->getType() === 'group') && ($elgg_file->getContainerEntity()->getGUID() == (int)$elgg_group->guid)) {
 			echo '......'.$elgg_file->getFilename()." | ".$elgg_file->size()." bytes"." <a href='".$url."'> X </a><br/>";
 			echo '..........'.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated()." | ".$elgg_file->getContainerEntity()->name."<br/>";
+		
+			if (!$ext_count[$ext])
+				$ext_count[$ext] = 1;
+			else
+				$ext_count[$ext]++;
 		}
 	}
-}
+
+	$ext_name = array();
+	$data = array();
+	foreach ($ext_count as $extension => $total_ext) {
+		$ext_name[] = $extension."\n%.1f%%";
+		$data[] = $total_ext;
+	}
+
+	if ($data) {
+		// Create the Pie Graph. 
+		$graph = new PieGraph(350,250);
+
+		$theme_class="DefaultTheme";
+		//$graph->SetTheme(new $theme_class());
+
+		// Set A title for the plot
+		$graph->title->Set("");
+		$graph->SetBox(true);
+
+		// Create
+		$p1 = new PiePlot($data);
+		$p1->SetLabelType(PIE_VALUE_PER);
+		$p_lbl = $ext_name;
+		$p1->SetLabels($p_lbl);
+		$graph->Add($p1);
+
+		$p1->ShowBorder();
+		$p1->SetColor('black');
+		$p1->SetSliceColors(array('#800000','#339933','#99ff66','#ff6699','#0099ff'));
+
+		$fp = elgg_get_plugins_path()."c_dashboard_usage/img/pie_chart-".$num_pie.".png";
+		$file = $graph->Stroke($fp);
+		echo '<br/><img src="'.elgg_get_site_url().'mod/c_dashboard_usage/img/pie_chart-'.$num_pie.'.png"><br/>';
+		$num_pie++;
+	} else {
+		echo '<center><strong><p>No Data Available</p></strong></center>';
+	}
+	
+} // end loop for groups
+
 echo "<hr>";
+
+
+
 
 
 
@@ -149,7 +275,7 @@ echo $site_entities[2];
 echo "<hr>";
 foreach ($elgg_users as $elgg_user) {
 	echo "..<strong>".(int)$elgg_user->guid." - ".$elgg_user->username."</strong><br/>";
-
+	$ext_count = array();
 	// get all files
 	$elgg_files = elgg_get_entities(array(
 		'subtype' => 'file',
@@ -158,123 +284,102 @@ foreach ($elgg_users as $elgg_user) {
 	));
 
 	foreach ($elgg_files as $elgg_file) {
+		$url = elgg_add_action_tokens_to_url(elgg_get_site_url().'action/file/delete?guid='.$elgg_file->getGUID());
+		$ext = pathinfo($elgg_file->getFilename(), PATHINFO_EXTENSION);
 		if (($elgg_file->getContainerEntity()->getType() === 'user') && ($elgg_file->getContainerEntity()->getGUID() == (int)$elgg_user->guid)) {
 			echo '......'.$elgg_file->getFilename()." | ".$elgg_file->size()." bytes"." <a href='".$url."'> X </a><br/>";
-			echo '..........'.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated()." | ".$elgg_file->getContainerEntity()->name."<br/>";
-
+			echo '..........'.$ext.' | '.$elgg_file->getOwnerEntity()->username." | <a href='".$elgg_file->getURL()."'>File Location</a> | ".$elgg_file->getTimeUpdated()." | ".$elgg_file->getContainerEntity()->name."<br/>";
+			if (!$ext_count[$ext])
+				$ext_count[$ext] = 1;
+			else
+				$ext_count[$ext]++;
 		}
+	}
+
+	$ext_name = array();
+	$data = array();
+	foreach ($ext_count as $extension => $total_ext) {
+		$ext_name[] = $extension."\n%.1f%%";
+		$data[] = $total_ext;
+	}
+	
+	if ($data) {
+		// Create the Pie Graph. 
+		$graph = new PieGraph(350,250);
+
+		$theme_class="DefaultTheme";
+		//$graph->SetTheme(new $theme_class());
+
+		// Set A title for the plot
+		$graph->title->Set("");
+		$graph->SetBox(true);
+
+		// Create
+		$p1 = new PiePlot($data);
+		$p1->SetLabelType(PIE_VALUE_PER);
+		$p_lbl = $ext_name;
+		$p1->SetLabels($p_lbl);
+		$graph->Add($p1);
+
+		$p1->ShowBorder();
+		$p1->SetColor('black');
+		$p1->SetSliceColors(array('#800000','#339933','#99ff66','#ff6699','#0099ff'));
+
+		$fp = elgg_get_plugins_path()."c_dashboard_usage/img/pie_chart-".$num_pie.".png";
+		$file = $graph->Stroke($fp);
+		echo '<br/><img src="'.elgg_get_site_url().'mod/c_dashboard_usage/img/pie_chart-'.$num_pie.'.png"><br/>';
+		$num_pie++;
+	} else {
+		echo '<center><strong><p>No Data Available</p></strong></center>';
 	}
 }
 echo "<br/><br/>";
 
-elgg_load_css('collap_tree');
+
 ?>
 
 
 
 <!-- Testing Collapsible Tree List (Visually appealing) -->
-
 <ol class="tree">
-		<li>
-			<label>dsfsd</label>
-			 <input type="checkbox" checked disabled id="folder1" /> 
-			<ol>
-				<li class="file"><a href="document.html.pdf">File 1</a></li>
-				<li>
-					<label for="subfolder1">Subfolder 1</label> <input type="checkbox" id="subfolder1" /> 
-					<ol>
-						<li class="file"><a href="">Filey 1</a></li>
-						<li>
-							<label for="subsubfolder1">Subfolder 1</label> <input type="checkbox" id="subsubfolder1" /> 
-							<ol>
-								<li class="file"><a href="">File 1</a></li>
-								<li>
-									<label for="subsubfolder2">Subfolder 1</label> <input type="checkbox" id="subsubfolder2" /> 
-									<ol>
-										<li class="file"><a href="">Subfile 1</a></li>
-										<li class="file"><a href="">Subfile 2</a></li>
-										<li class="file"><a href="">Subfile 3</a></li>
-										<li class="file"><a href="">Subfile 4</a></li>
-										<li class="file"><a href="">Subfile 5</a></li>
-										<li class="file"><a href="">Subfile 6</a></li>
-									</ol>
-								</li>
-							</ol>
-						</li>
-						<li class="file"><a href="">File 3</a></li>
-						<li class="file"><a href="">File 4</a></li>
-						<li class="file"><a href="">File 5</a></li>
-						<li class="file"><a href="">File 6</a></li>
-					</ol>
-				</li>
-			</ol>
-		</li>
-		<li>
-			<label for="folder2">Folder 2</label> <input type="checkbox" id="folder2" /> 
-			<ol>
-				<li class="file"><a href="">File 1</a></li>
-				<li>
-					<label for="subfolder2">Subfolder 1</label> <input type="checkbox" id="subfolder2" /> 
-					<ol>
-						<li class="file"><a href="">Subfile 1</a></li>
-						<li class="file"><a href="">Subfile 2</a></li>
-						<li class="file"><a href="">Subfile 3</a></li>
-						<li class="file"><a href="">Subfile 4</a></li>
-						<li class="file"><a href="">Subfile 5</a></li>
-						<li class="file"><a href="">Subfile 6</a></li>
-					</ol>
-				</li>
-			</ol>
-		</li>
-		<li>
-			<label for="folder3">Folder 3</label> <input type="checkbox" id="folder3" /> 
-			<ol>
-				<li class="file"><a href="">File 1</a></li>
-				<li>
-					<label for="subfolder3">Subfolder 1</label> <input type="checkbox" id="subfolder3" /> 
-					<ol>
-						<li class="file"><a href="">Subfile 1</a></li>
-						<li class="file"><a href="">Subfile 2</a></li>
-						<li class="file"><a href="">Subfile 3</a></li>
-						<li class="file"><a href="">Subfile 4</a></li>
-						<li class="file"><a href="">Subfile 5</a></li>
-						<li class="file"><a href="">Subfile 6</a></li>
-					</ol>
-				</li>
-			</ol>
-		</li>
-		<li>
-			<label for="folder4">Folder 4</label> <input type="checkbox" id="folder4" /> 
-			<ol>
-				<li class="file"><a href="">File 1</a></li>
-				<li>
-					<label for="subfolder4">Subfolder 1</label> <input type="checkbox" id="subfolder4" /> 
-					<ol>
-						<li class="file"><a href="">Subfile 1</a></li>
-						<li class="file"><a href="">Subfile 2</a></li>
-						<li class="file"><a href="">Subfile 3</a></li>
-						<li class="file"><a href="">Subfile 4</a></li>
-						<li class="file"><a href="">Subfile 5</a></li>
-						<li class="file"><a href="">Subfile 6</a></li>
-					</ol>
-				</li>
-			</ol>
-		</li>
-		<li>
-			<label for="folder5">Folder 5</label> <input type="checkbox" id="folder5" /> 
-			<ol>
-				<li class="file"><a href="">File 1</a></li>
-				<li>
-					<label for="subfolder5">Subfolder 1</label> <input type="checkbox" id="subfolder5" /> 
-					<ol>
-						<li class="file"><a href="">Subfile 1</a></li>
-						<li class="file"><a href="">Subfile 2</a></li>
-						<li class="file"><a href="">Subfile 3</a></li>
-						<li class="file"><a href="">Subfile 4</a></li>
-						<li class="file"><a href="">Subfile 5</a></li>
-						<li class="file"><a href="">Subfile 6</a></li>
-					</ol>
-				</li>
-			</ol>
-		</li>
-	</ol>
+
+
+<li>
+	<label>Site1</label><input type="checkbox" id="folder1" />
+		<ol>
+			<li>
+				<label for="subsubfolder2">File</label> <input type="checkbox" id="subsubfolder2" />
+				<ol>
+					<li class="file"><a href="">File Info</a></li>
+				</ol>
+			</li>
+			<li>
+				<label for="subsubfolder2">File</label> <input type="checkbox" id="subsubfolder2" />
+				<ol>
+					<li class="file"><a href="">File Info</a></li>
+				</ol>
+			</li>
+		</ol>
+	</li>
+
+<li>
+	<label>Site1</label><input type="checkbox" id="folder1" />
+		<ol>
+			<li>
+				<label for="subsubfolder2">File</label> <input type="checkbox" id="subsubfolder2" />
+				<ol>
+					<li class="file"><a href="">File Info</a></li>
+				</ol>
+			</li>
+			<li>
+				<label for="subsubfolder2">File</label> <input type="checkbox" id="subsubfolder2" />
+				<ol>
+					<li class="file"><a href="">File Info</a></li>
+				</ol>
+			</li>
+		</ol>
+	</li>
+
+</ol>
+
+<br/><br/>
